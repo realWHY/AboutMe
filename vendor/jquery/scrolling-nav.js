a464503979a6d200d9fa6f60de61de457cd22ce6
realWHY <sm10547@yahoo.com.tw>
2017-08-15 20:19:28 +0800

    upload

--- vendor/jquery/scrolling-nav.js
+++ vendor/jquery/scrolling-nav.js
@@ -11,3 +11,22 @@ $(function() {
         event.preventDefault();
     });
 });
+
+$(function(){
+	
+	//Check to see if the window is top if not then display button
+	$(window).scroll(function(){
+		if ($(this).scrollTop() > 400) {
+			$('.scrollToTop').fadeIn();
+		} else {
+			$('.scrollToTop').fadeOut();
+		}
+	});
+	
+	//Click event to scroll to top
+	$('.scrollToTop').click(function(){
+		$('html, body').animate({scrollTop : 0},600);
+		return false;
+	});
+	
+});
